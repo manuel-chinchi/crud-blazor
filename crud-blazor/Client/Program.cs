d1c4a00bbb278b4b271f695f53f2317d04a8c02b
Manuel Esteban Chinchi Garcia <manuel.chinchi93@gmail.com>
2024-08-01 19:27:50 -0300

    add package Seetalert2 v5.6.0 and popup message on delete components

--- crud-blazor/Client/Program.cs
+++ crud-blazor/Client/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +19,7 @@ namespace crud_blazor.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSweetAlert2();
 
             await builder.Build().RunAsync();
         }

12984f3471b83fb9b9d60e51f5eef2acb97d8ec3
Manuel Chinchi <manuel.chinchi93@gmail.com>
2025-11-23 15:01:16 -0300

    [Client] add timestamp to the output export file name

--- crud-blazor/Client/wwwroot/js/utils.js
+++ crud-blazor/Client/wwwroot/js/utils.js
@@ -6,3 +6,9 @@
     link.click();
     document.body.removeChild(link);
 }
+
+window.getLocalTimestamp = () => {
+    const d = new Date();
+    const pad = n => n.toString().padStart(2, '0');
+    return `${pad(d.getDate())}-${pad(d.getMonth() + 1)}-${d.getFullYear()}_${pad(d.getHours())}-${pad(d.getMinutes())}-${pad(d.getSeconds())}`;
+};
